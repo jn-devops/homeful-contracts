f4e77a8a5114668b318310f1aca31abd8a4b2e2b
Christian Quelonio <cequelonio@joy-nostalg.com>
2025-02-07 15:31:45 +0800

    update UI on upload documents

--- resources/views/livewire/requirements-table.blade.php
+++ resources/views/livewire/requirements-table.blade.php
@@ -35,13 +35,23 @@
                     <td class="fi-ta-cell px-4 py-2 text-sm text-gray-900 dark:text-gray-300">
                         {{ $loop->iteration }}
                     </td>
-                    <td class="fi-ta-cell px-4 py-2 text-sm text-gray-900 dark:text-gray-300">
-                        {{ $requirement['status'] }} <!-- Default to 'Pending' if status is missing -->
+                    <td class="fi-ta-cell px-4 py-2 text-sm text-gray-900 dark:text-gray-300 flex gap-2 items-center">
+                        @if ($requirement['status'] == 'Uploaded')
+                            <svg class="w-4 h-4 text-green-900" style="color: rgb(20 83 45 / var(--tw-text-opacity, 1));" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z" clip-rule="evenodd"/>
+                            </svg>
+                            {{ $requirement['status'] }}
+                        @else
+                            <svg class="w-4 h-4 text-orange-600" style="color: rgb(234 88 12 / var(--tw-text-opacity, 1));" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clip-rule="evenodd"/>
+                            </svg>
+                            {{ $requirement['status'] }}
+                        @endif
                     </td>
                     <td class="fi-ta-cell px-4 py-2 text-sm text-gray-900 dark:text-gray-300">
                         {{ $requirement['description'] }}
                     </td>
-                    <td class="fi-ta-cell px-4 py-2 text-sm text-gray-900 dark:text-gray-300 flex flex-row items-center gap-4">
+                    <td class="fi-ta-cell px-4 py-2 text-sm text-gray-900 dark:text-gray-300 flex flex-row items-center justify-center gap-4">
                         <input type="file" id="documentInput{{$record->id}}" wire:model="document" class="hidden" />
                         <div class="cursor-pointer" wire:click="viewImage('{{ $requirement['description'] }}')">
                             <svg width="16" height="13" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
