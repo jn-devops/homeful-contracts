a3b7cb79c3d2b7a6e0542c289aae0b41f558620c
RDC15 <34361260+renzoDC15@users.noreply.github.com>
2025-02-17 15:25:42 +0800

    add gate viewPulse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\Number;
@@ -21,6 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::define('viewPulse', function (User $user) {
+            //implement user based access
+            return true;
+        });
+
         Number::useCurrency('PHP');
         Vite::prefetch(concurrency: 3);
     }
