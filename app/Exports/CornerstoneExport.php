5653141cbcd227fd72dc252da2adfa91e770d8a2
jn-devops <devops@joy-nostalg.com>
2025-02-28 15:38:50 +0800

    add headings to cornerstone export

--- app/Exports/CornerstoneExport.php
+++ app/Exports/CornerstoneExport.php
@@ -2,20 +2,21 @@
 
 namespace App\Exports;
 
-use Homeful\Contracts\Data\ContractData;
-use Homeful\Contracts\Models\Contract;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Homeful\Contracts\Data\ContractData;
 use Spatie\LaravelData\DataCollection;
+use Homeful\Contracts\Models\Contract;
 
-class CornerstoneExport implements FromCollection
+class CornerstoneExport implements FromCollection, WithHeadings
 {
     protected array $uuids;
 
     public function __construct(array $uuids)
     {
         $this->uuids = $uuids;
-    }
 
+    }
 
     public function collection()
     {
@@ -28,10 +29,27 @@ class CornerstoneExport implements FromCollection
         // Transform the collection to pick only required fields
         return $dataCollection->toCollection()->map(function (ContractData $contract) {
             return [
-                'First Name' => $contract->customer?->first_name ?? 'N/A',
-                'Last Name' => $contract->customer?->last_name ?? 'N/A',
-                'Civil Status' => $contract->customer?->civil_status?->value ?? 'N/A',
+                'FirstName' => $contract->customer?->first_name ?? 'N/A',
+                'MiddleName' => $contract->customer?->middle_name ?? 'N/A',
+                'LastName' => $contract->customer?->last_name ?? 'N/A',
+                'CivilStatus' => $contract->customer?->civil_status?->value ?? 'N/A',
+                'Sex' => $contract->customer?->sex?->value ?? 'N/A',
+                'DateOfBirth' => $contract->customer->date_of_birth->format('Y-m-d'),
+                'Ownership' => $contract->customer->addresses->first()->ownership->value
             ];
         });
+    }
+
+    public function headings(): array
+    {
+       return [
+           'FirstName',
+           'MiddleName',
+           'LastName',
+           'CivilStatus',
+           'Sex',
+           'DateOfBirth',
+           'Ownership'
+       ];
     }
 }
