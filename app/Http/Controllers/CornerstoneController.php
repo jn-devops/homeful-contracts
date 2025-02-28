b3e34756034a900889eb97284e7ba26de121808d
RDC15 <34361260+renzoDC15@users.noreply.github.com>
2025-02-28 16:25:14 +0800

    get all Contract for Export

--- app/Http/Controllers/CornerstoneController.php
+++ app/Http/Controllers/CornerstoneController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Homeful\Contracts\Models\Contract;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\CornerstoneExport;
 use Illuminate\Http\Request;
@@ -14,6 +15,8 @@ class CornerstoneController extends Controller
     public function __invoke(Request $request)
     {
         $uuids = ['220b6576-f91f-4f96-bc36-13654f3c4885'];
+
+        $uuids = Contract::pluck('id')->toArray();
 
         return Excel::download(new CornerstoneExport($uuids), 'cornerstone.xlsx');
     }
