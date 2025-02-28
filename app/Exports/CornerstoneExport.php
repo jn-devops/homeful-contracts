f3a3cb576e395971ee962983875a865e5fcfbdde
RDC15 <34361260+renzoDC15@users.noreply.github.com>
2025-02-28 16:11:29 +0800

    update fields on CornerstoneExport

--- app/Exports/CornerstoneExport.php
+++ app/Exports/CornerstoneExport.php
@@ -35,7 +35,24 @@ class CornerstoneExport implements FromCollection, WithHeadings
                 'CivilStatus' => $contract->customer?->civil_status?->value ?? 'N/A',
                 'Sex' => $contract->customer?->sex?->value ?? 'N/A',
                 'DateOfBirth' => $contract->customer->date_of_birth->format('Y-m-d'),
-                'Ownership' => $contract->customer->addresses->first()->ownership->value
+                'Ownership' => $contract->customer->addresses->first()->ownership->value,
+                'Location' => $contract->customer->addresses->first()->address,
+                'Type' => $contract->customer->addresses->first()->type ?? 'N/A',
+                'BlockLot'=>'',
+                'Street' => '',
+                'Barangay' => $contract->customer->addresses->first()->sublocality ?? '',
+                'City' => $contract->customer->addresses->first()->locality ?? '',
+                'Province' => $contract->customer->addresses->first()->administrativ_area ?? '',
+                'State'=>$contract->customer->addresses->first()->region ?? '',
+                'Country' => $contract->customer->addresses->first()->country ?? '',
+                'ZipCode' => $contract->customer->addresses->first()->postal_code ?? '',
+                'Primary'=>'',
+                'Salary'=>$contract->customer->employment->first()->monthly_gross_income ?? '',
+                'GrossIncome'=>$contract->customer->employment->first()->monthly_gross_income ?? '',
+                'Nationality'=>$contract->customer->nationality->value ?? '',
+                'SellerEmail'=> '',
+                'TIN'=>$contract->customer->employment->first()->id->tin??'',
+                'Pag-ibig Number'=> $contract->customer->employment->first()->id->pagibig ?? '',
             ];
         });
     }
