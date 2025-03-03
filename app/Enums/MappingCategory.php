a82019ca7662bcee497939c7214d940cd6d3bf6d
Christian Quelonio <cequelonio@joy-nostalg.com>
2025-03-03 13:08:48 +0800

    updated the upload documents in cad/cfu

--- app/Enums/MappingCategory.php
+++ app/Enums/MappingCategory.php
@@ -17,6 +17,8 @@ enum MappingCategory: string
     case SYSTEM = 'system';
 
     case MORTGAGE = 'mortgage';
+    case CO_BORROWER = 'co_borrowers';
+    case AIF = 'aif';
 
     static function default(): self {
         return self::BUYER;
