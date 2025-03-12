b37a32ba9364717cb2ed78d3f862389418514ee2
Anais Santos <anaiszx@Anaiss-MacBook-Air.local>
2025-03-12 11:21:39 +0800

    add horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     Homeful\Contracts\Providers\EventServiceProvider::class,
     Homeful\Mortgage\Providers\EventServiceProvider::class,
     Spatie\SchemalessAttributes\SchemalessAttributesServiceProvider::class,
